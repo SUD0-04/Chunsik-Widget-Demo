2757d2ac51caa893a23da79da07d513ea48eeebb
Arnav <umi0236@icloud.com>
2023-09-30 14:39:54 +0900

    CHUNSIK WIDGET For Demo
    Changes
    - 위젯의 메인 화면에 설명을 넣었습니다.

--- TimeWidget/ContentView.swift
+++ TimeWidget/ContentView.swift
@@ -10,8 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("")
-            Text("Hello, world!")
+            Text("Chunsik Widget")
+                .font(.largeTitle)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            Text("For Demo")
+                .padding(.bottom, 500)
+            Text("본 앱은 Widget Kit 구현을 연습하기 위해 개발되었습니다.\n위젯을 추가하려면 배경화면에서 TimeWidget을 찾아 추가해주세요")
+                .font(.system(size: 12))
+                .multilineTextAlignment(.center)
         }
         .padding()
     }
